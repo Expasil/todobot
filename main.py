d271cc84d4e1a5495c2bc9e44fc011b50786079d
Arseniy <arsenij08022001@gmail.com>
2023-07-21 15:08:22 +0600

    add some comments

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import db
-API_TOKEN = ''
+API_TOKEN = ''#Вставьте свой токен из BotFather Сюда
 
 
 Storage=MemoryStorage()
